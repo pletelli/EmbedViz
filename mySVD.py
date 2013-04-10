0f61487f63bd0fa7e40ee55806cca0be65101e68
Perrine <arkzyna@gmail.com>
2013-04-10 15:59:50 +0200

    After seeing A. Bordes and N.Usunier 9/04

--- mySVD.py
+++ mySVD.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 import numpy as np
+import scipy.spatial.distance as sc
 import matplotlib.pyplot as plt
 from rsvd import RSVD, rating_t, MovieLensDataset
 
@@ -17,18 +18,19 @@ v = int(train.shape[0]*0.9)
 val = train[v:]
 train = train[:v]
 
+
 dims = (dataset.movieIDs().shape[0], dataset.userIDs().shape[0])
 
 # boucle pour iterer sur reg
 factor = 40
 
 i = 0
-alphas = []
+lambdas = []
 errors = []
-for alpha in np.arange(0, 1, 0.005): 
-# alpha ne doit pas depasser 1
+for lambda_f in np.arange(0.00, 0.03, 0.001): 
+# lambda_f ne doit pas depasser 1
 # masEpochs = 1000
-	model = RSVD.train(factor, train, dims, probeArray=val, maxEpochs = 500, regularization=alpha)
+	model = RSVD.train(factor, train, dims, probeArray=val, maxEpochs = 1000, regularization=lambda_f)
 
 	sqerr=0.0
 	for movieID,userID,rating in test:
@@ -38,27 +40,55 @@ for alpha in np.arange(0, 1, 0.005):
 
 
 	print "-------------------------------------------------"
-	print "Pour alpha = ",alpha, " Test RMSE: ", np.sqrt(sqerr)
+	print "Pour lambda = ",lambda_f, " Test RMSE: ", np.sqrt(sqerr)
 	print "-------------------------------------------------"
-	alphas.append(alpha)
-	errors.append(sqerr)
+	lambdas.append(lambda_f)
+	errors.append(np.sqrt(sqerr))
 
-
-print alphas
+# print the lamdas and errors vectors
+print lambdas
 print errors
-#print " alpha | erreur "
+
+# get minimal error and its corresponding lamda
+min_err = min(errors)
+id_min_err = errors.index(min(errors))
+best_lambda = lambdas[errors.index(min(errors))]
+print "minimum trouve pour l erreur", min_err
+print "correspond a lambda =", best_lambda
+
+
+#print " lambda | erreur "
 #print "________________"
 #i =0
-#while i < len(alphas) & i < len(errors):
+#while i < len(lambdas) & i < len(errors):
 	#print i
-	#print alphas[i], " | " , errors[i]
+	#print lambdas[i], " | " , errors[i]
 
-plt.plot(alphas, errors)
+
+#plot errors /lambdas
+plt.plot(lambdas, errors)
 plt.ylabel('erreur')
-plt.xlabel('alpha')
+plt.xlabel('lambda')
 plt.show()
+
+#model.u c est une matrice avec un vecteur par film
+#model.v c est une matrice avec un vecteur par utilisateur
+
+dist = sc.pdist(model.u)
+dissim = sc.squareform(dist)
+# on obtient une matrice de dissimilarite
+
+
+
+
+#calcul de norme, retrouver le film associe
+
 # extraire le nombre d iterations
-# 
+# visualiser avec un modele pourri
+# caraceristique films (note moyenne, genre, nombre de personne l'ayant noté, type des gens l'ayant noté)
+#selecetion des films a visu en focntion du nombre de note, faible, pas
+# 5OO objet cest bien a visu
+# restreindre lapprentisage aux gens dun certain age/sexe
 
 
 
